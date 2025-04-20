17494321cd1cf8c7d525e686a8784efabd3098ab
Zabi Sideqi <zabi@technspire.com>
2025-04-20 17:03:48 +0200

    Create Mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.Mongo)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 ');
