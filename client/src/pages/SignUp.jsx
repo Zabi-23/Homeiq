b008c1ab931cb2bae859e6815961b522c767e382
Zabi Sideqi <zabi@technspire.com>
2025-04-21 22:31:57 +0200

    complete sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div> Sign-Up</div>
+    <div className='p-3 max-w-lg mx-auto'>
+       <h1 className='text-3xl text-center font-semibold my-7'>Sign-Up</h1>
+        <form className='flex flex-col gap-4'>
+          <input type="text" placeholder='Enter your name' 
+          className='  bg-stone-50 focus:outline-none p-3 rounded-lg' id= 'username'/>
+
+          <input type="email" placeholder='Enter your email'
+           className=' bg-stone-50 focus:outline-none p-3 rounded-lg ' id= 'email'/>
+
+          <input type="password" placeholder='Enter your password' 
+          className=' bg-stone-50 focus:outline-none  p-3 rounded-lg ' id= 'password'/>
+
+          <button className='bg-slate-600 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-75 p-3 '>Sign Up</button>
+        </form>
+         
+         {/* alrdy have an account */}
+          <p className='text-center mt-4'>Already have an account? 
+            <a href="/signin" className='text-slate-600 hover:underline'>Sign In</a></p>
+
+       </div>
   )
 }
 
