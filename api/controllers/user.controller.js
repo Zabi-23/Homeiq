9c3f3b7a2dfa8b8a3cb96a1afa18c6af650a6385
Zabi Sideqi <zabi@technspire.com>
2025-05-05 22:46:42 +0200

    Create Get user listing api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -42,6 +42,8 @@ export const updateUser = async (req, res, next) => {
 import bcrypt from "bcryptjs";  
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import e from "express";
+import Listing from "../models/listing.model.js";
 
 // Test route
 export const test = (req, res) => {
@@ -119,4 +121,20 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
       next(error);
   }
+}
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings)
+      
+    } catch (error) {
+      next(error);
+    }
+  } else{
+
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+  
 }
--- api/routes/user.rout.js
+++ api/routes/user.rout.js
@@ -1,8 +1,8 @@
 //api/routes/user.rout.js
 import express from 'express';
-import { updateUser, test } from '../controllers/user.controller.js';
+import { updateUser, test, getUserListing, deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteUser } from '../controllers/user.controller.js';
+
 
 const router = express.Router();
 
@@ -10,7 +10,8 @@ router.get('/test', test);
 router.put('/update/:id', verifyToken, updateUser);
 /* router.delete('/delete/:id', verifyToken, deleteUser); */
 
-//delete user just by id without token
+router.get('/listing/:id', verifyToken, getUserListing); //get user listing by id)
+//delete user just by id without token activate token before production
 router.delete('/delete/:id', deleteUser);
 
 export default router;
