559aa26c9bf38cd53abda3596833d16dc0d81e90
Zabi Sideqi <zabi@technspire.com>
2025-05-06 17:01:54 +0200

    add image slider to listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,62 @@
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore from 'swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/bundle';
+export default function Listing() {
+     SwiperCore.use([Navigation]);
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const params = useParams();
+
+    useEffect(() => {
+        const fetchListing = async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`/api/listing/get/${params.listingId}`);
+                const data = await res.json();
+                if (data.success === false) {
+                    setError(true);
+                    setLoading(false);
+                    return;
+                }
+                setListing(data);
+                setLoading(false); // ✅ Lägg till denna
+            } catch { // ✅ rätt fånga error
+                setError(true);
+                setLoading(false);
+            }
+        };
+
+        fetchListing();
+    }, [params.listingId]); // ✅ rätt dependencies
+
+    return (
+        <main>
+            {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+            {error && <p className='text-center my-7 text-2xl'>Error loading listing.</p>}
+            {listing && !loading && !error && (
+                <>
+                    <Swiper navigation>
+                        {listing.imageUrls.map(url => 
+                            <SwiperSlide key={url}>
+                                <div className='h-[500px]' style={{background:`url(${url}) center no-repeat`
+                            , backgroundSize: 'cover'}}></div>
+                               
+
+                                
+                            </SwiperSlide>
+                        )}
+                    </Swiper>
+                </>
+               
+            )}
+        </main>
+    );
+}
+
+  
