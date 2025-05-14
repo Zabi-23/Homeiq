8b895ebf065638df2092efcfb4658bf29e09c772
Zabi Sideqi <zabi@technspire.com>
2025-05-14 15:06:56 +0200

    create new user.router.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 //api/routes/user.rout.js
-import express from 'express';
+/* import express from 'express';
 import { updateUser, test, getUserListing, deleteUser, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -8,12 +8,28 @@ const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:id', verifyToken, updateUser);
-/* router.delete('/delete/:id', verifyToken, deleteUser); */
+ // router.delete('/delete/:id', verifyToken, deleteUser); 
 
-/* router.get('/listing/:id', verifyToken, getUserListing); */ //get user listing by id)
+// router.get('/listing/:id', verifyToken, getUserListing);  //get user listing by id)
 router.get('/listing/:id', getUserListing);
 //delete user just by id without token activate token before production
 router.delete('/delete/:id', deleteUser);
 router.get('/:id', verifyToken, getUser); //get user by id with token
 
+export default router; */
+
+// api/routes/user.route.js
+import express from 'express';
+import { updateUser, test, getUserListing, deleteUser, getUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.get('/test', test);
+router.put('/update/:id', verifyToken, updateUser);
+router.get('/listing/:id', getUserListing);
+router.delete('/delete/:id', deleteUser);
+router.get('/user/:id', verifyToken, getUser); // <-- fixad här
+
 export default router;
+
